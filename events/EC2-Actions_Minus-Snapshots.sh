249085fc9f3dd53aa8442847c724b6ddf7893159
JoeV <15126922+josephedward@users.noreply.github.com>
2022-04-16 22:36:50 -0400

    Update EC2-Actions_Minus-Snapshots.sh

--- events/EC2-Actions_Minus-Snapshots.sh
+++ events/EC2-Actions_Minus-Snapshots.sh
@@ -1,14 +1,14 @@
 aws events put-rule --name "EC2-Actions_Minus-Snapshots" --event-pattern '
 {
   "source": ["aws.ec2"],
-  "source": [{
-    "anything-but": ["snapshot"]
-  }]
+   "detail": {
+    "state": [ { "anything-but": { "prefix": "snapshot" } } ]
+  }
 }'
 
-aws sns create-topic --name EBS-EC2-VPC_Actions_Minus-Snapshots
+aws sns create-topic --name EC2-Actions_Minus-Snapshots
 
-aws events put-targets --rule EBS-EC2-VPC_Actions_Minus-Snapshots --targets "Id"="1","Arn"=$(aws sns list-topics --query 'Topics[0].TopicArn' --output text)
+aws events put-targets --rule EC2-Actions_Minus-Snapshots --targets "Id"="1","Arn"=$(aws sns list-topics --query 'Topics[0].TopicArn' --output text)
 
 aws sns subscribe --topic-arn $(aws sns list-topics --query 'Topics[0].TopicArn' --output text) --protocol email --notification-endpoint josephedwardwork@gmail.com
 
